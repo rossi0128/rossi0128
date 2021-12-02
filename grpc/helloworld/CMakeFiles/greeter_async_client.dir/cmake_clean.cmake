33532a578de0373a7f020602a97921e0c61ae40f
rossi0128 <woshikjt@163.com>
2021-12-02 17:55:46 +0800

    update grpc_helloword

---
+++ grpc/helloworld/CMakeFiles/greeter_async_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greeter_async_client.dir/greeter_async_client.cc.o"
+  "bin/greeter_async_client"
+  "bin/greeter_async_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greeter_async_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ grpc/helloworld/CMakeFiles/greeter_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greeter_server.dir/greeter_server.cc.o"
+  "bin/greeter_server"
+  "bin/greeter_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greeter_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
